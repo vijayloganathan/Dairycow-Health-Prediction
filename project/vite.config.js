f71882d86fb4203a883d15d52d97bffc93e72b27
vijayloganathan <vijayloganathan2002@gmail.com>
2024-09-23 07:23:48 +0530

    changes in vite.json file

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import commonjs from '@rollup/plugin-commonjs'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    commonjs() // This will transform CommonJS modules to ES modules
+  ],
 })
